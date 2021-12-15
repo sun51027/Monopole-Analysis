9a08aa0dcb1e0bbfa8eab4a9221567d557655349
Shih <arashikattun48@gmail.com>
2021-12-15 17:49:05 +0800

    _

--- copy.sh
+++ copy.sh
@@ -1,9 +1,24 @@
-#!/bin/bash
 set -e  #if there is error , stop running
-#scp -rp lshih@lxplus.cern.ch:~/CMSSW_10_6_8_patch1/src/Monopoles . 
+#scp -rp lshih@lxplus.cern.ch:~/CMSSW_10_6_23/src/Monopoles .
 #scp -rp lshih@lxplus.cern.ch:~/test1/CMSSW_8_0_33_UL/src/linshih_crab/crab_Monopole_SpinHalf_M-1000_DY_13TeV_v1/ ./MonoNtuple2016/
 #scp -rp lshih@lxplus.cern.ch:~/OldestTest/CMSSW_10_6_8_patch1/src/MonopoleNtuple1000_2017_1000evt.root .
 #scp -rp lshih@lxplus.cern.ch:/eos/user/l/lshih/MonoNtuple2018_MC_1000.root ./SmallerData/
-#scp -rp lshih@lxplus.cern.ch:/eos/user/l/lshih/MonoNtuple2018_MC_3000.root .
-#scp -rp lshih@lxplus.cern.ch:/eos/user/l/lshih/MonoNtuple2018_MC_2000.root .
-scp -rp lshih@lxplus.cern.ch:/eos/user/l/lshih/MonoNtuple2018_MC_1000_TestSwisscross2.root .
+#scp -rp lshih@lxplus.cern.ch:/eos/user/l/lshih/MCNtuple2018/4500 MCNtuple2018
+#scp -rp lshih@lxplus.cern.ch:/eos/user/l/lshih/MCNtuple2016 .
+#rsync -avh  lshih@lxplus.cern.ch:/eos/user/l/lshih/MCNtuple2016 .
+#scp -rp lshih@lxplus.cern.ch:/eos/user/l/lshih/MCNtuple2018/2500 MCNtuple2018
+#scp -rp lshih@lxplus.cern.ch:/eos/user/l/lshih/Systematic/Ecal/2017/ Systematic/Ecal/
+#scp -rp lshih@lxplus.cern.ch:/eos/user/l/lshih/Systematic/Ecal/2016/ Systematic/Ecal/
+#scp -rp lshih@lxplus.cern.ch:/eos/user/l/lshih/Systematic/Ecal/2016APV/ Systematic/Ecal/
+#scp -rp lshih@lxplus.cern.ch:/eos/user/l/lshih/Systematic/DedxCrossTalk/2016APV Systematic/DedxCrossTalk/
+#scp -rp lshih@lxplus.cern.ch:/eos/user/l/lshih/Systematic/DedxCrossTalk/2016 Systematic/DedxCrossTalk/
+#scp -rp lshih@lxplus.cern.ch:/eos/user/l/lshih/Systematic/DedxCrossTalk/2017 Systematic/DedxCrossTalk/
+scp -rp lshih@lxplus.cern.ch:/eos/user/l/lshih/Systematic/DedxCrossTalk/2018/X0/down/4000/ Systematic/DedxCrossTalk/2018/X0/down/
+#scp -rp lshih@lxplus.cern.ch:/eos/user/l/lshih/Systematic/DeltaRayOff/2016/2000/ Systematic/DeltaRayOff/2016/2000/
+#scp -rp lshih@lxplus.cern.ch:/afs/cern.ch/user/l/lshih/CMSSW_10_6_23/src/ntuple_mc_2018_cfg.py .
+#scp -rp lshih@lxplus.cern.ch:/afs/cern.ch/user/l/lshih/CMSSW_10_6_23/src/tmpSUB.SUB .
+#scp -rp lshih@lxplus.cern.ch:/afs/cern.ch/user/l/lshih/CMSSW_10_6_23/src/submit.sh .
+#scp -rp lshih@lxplus.cern.ch:/eos/user/l/lshih/public .
+#scp -rp lshih@lxplus.cern.ch:/afs/cern.ch/user/l/lshih/CMSSW_10_6_23/src/Monopoles/MonoAnalysis/bin/monoNtupleAnalyzer.cc .
+#scp -rp lshih@lxplus.cern.ch:/afs/cern.ch/user/m/melsawy/public/ForLin/monoNtupleAnalyzer/monoNtupleAnalyzer.cc .
+
